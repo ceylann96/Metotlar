966deea71339314b9e6fd75b0fc64c470f87ff85
ceylann96 <91429246+ceylann96@users.noreply.github.com>
2022-08-15 16:15:18 +0300

    updated
    3. gün

---
+++ OOP2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri gercekMusteri = new GercekMusteri();
+            gercekMusteri.Id = 5;
+            gercekMusteri.MusteriNo = "1516156";
+            gercekMusteri.Adi = " Kadir";
+            gercekMusteri.Soyadi = "Cweylan";
+            gercekMusteri.TcNo = "105189911189";
+
+            TuzelMusteri tuzelMusteri = new TuzelMusteri();
+            tuzelMusteri.Id = 1599;
+            tuzelMusteri.SirketAdi = "TT";
+            tuzelMusteri.VergiNo = "105191";
+
+
+            Müsteri musteri3 = new GercekMusteri();
+            Müsteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(gercekMusteri);
+            musteriManager.Ekle(tuzelMusteri);
+
+
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            //ihtiyacKrediManager.Hesapla();
+
+            //TasitKrediManager tasitKrediManager= new TasitKrediManager();
+            //tasitKrediManager.Hesapla();
+
+            //KonutKrediManager konutKrediManager=new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+
+            //IKrediManager ihtiyacKrediManager1 = new IhtiyacKrediManager();
+            //ihtiyacKrediManager1.Hesapla();
+
+            //IKrediManager tasitKrediManager1 = new TasitKrediManager();
+            //tasitKrediManager1.Hesapla();
+
+            //IKrediManager konutKrediManager1 = new KonutKrediManager();
+            //konutKrediManager1.Hesapla();
+
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager efnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+            //List<ILoggerService> allOfTheLoggerService = new List<ILoggerService>() { smsLoggerService, databaseLoggerService, fileLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            
+            basvuruManager.BasvuruYap(efnafKrediManager, new List<ILoggerService> {databaseLoggerService, smsLoggerService });
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager };
+            
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
